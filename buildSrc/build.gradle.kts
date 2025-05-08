1dad826f41f43b7723ea7fa84a6b6132a5e9e313
PlatanoV3rde <franciscojaviercom59@gmail.com>
2025-05-08 02:04:26 -0500

    Add files via upload

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,34 @@
+import java.util.Properties
+
+plugins {
+    `kotlin-dsl`
+    kotlin("jvm") version embeddedKotlinVersion
+}
+
+repositories {
+    mavenCentral()
+    gradlePluginPortal()
+    maven {
+        name = "EngineHub"
+        url = uri("https://maven.enginehub.org/repo/")
+    }
+}
+
+val properties = Properties().also { props ->
+    project.projectDir.resolveSibling("gradle.properties").bufferedReader().use {
+        props.load(it)
+    }
+}
+
+dependencies {
+    implementation(gradleApi())
+    implementation("org.ajoberstar.grgit:grgit-gradle:5.2.1")
+    implementation("com.github.johnrengelman:shadow:8.1.1")
+    implementation("io.papermc.paperweight.userdev:io.papermc.paperweight.userdev.gradle.plugin:1.5.5")
+}
+
+kotlin {
+    jvmToolchain {
+        (this as JavaToolchainSpec).languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
